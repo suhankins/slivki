2b96461b72f2a4fdc6dea6f1d6f0525cde588d7e
suhankins <punishedbernadetta@niuitmo.ru>
2023-04-01 18:15:22 +0400

    better name changing for categories

--- src/views/CategoryEditable.tsx
+++ src/views/CategoryEditable.tsx
@@ -1,18 +1,18 @@
 import { CategoryClass } from '@/models/Category';
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import { Item } from './Item';
 
 export function CategoryEditable({ category }: { category: CategoryClass }) {
     const nameFieldRef = useRef<HTMLInputElement>(null);
-    const nameRef = useRef(category.name_en);
+    const [name, setName] = useState(category.name_en);
+    const [editing, setEditing] = useState(false);
     const [loading, setLoading] = useState(false);
 
     const updateCategory = () => {
         if (nameFieldRef.current) {
             const newName = nameFieldRef.current.value.trim();
-            if (newName !== nameRef.current && newName !== '') {
-                console.log(
-                    `Updating category from ${nameRef.current} to ${newName}`
-                );
+            if (newName !== name && newName !== '') {
+                console.log(`Updating category from ${name} to ${newName}`);
                 setLoading(true);
                 fetch(`/api/category/${category._id}`, {
                     body: JSON.stringify({ name_en: newName }),
@@ -22,29 +22,45 @@ export function CategoryEditable({ category }: { category: CategoryClass }) {
                     setLoading(false);
                     if (res.status === 200) {
                         console.log('Category updated');
-                        nameRef.current = newName;
+                        setName(newName);
                     } else {
                         console.error('Category update failed');
                     }
                 });
             } else {
-                nameFieldRef.current.value = nameRef.current;
+                nameFieldRef.current.value = name;
             }
         }
     };
 
+    useEffect(() => {
+        if (editing) nameFieldRef.current?.focus();
+    }, [editing]);
+
     return (
         <>
             <div className="divider">
+                <span
+                    className={`text-center text-xl font-bold ${
+                        (editing || loading) && 'hidden'
+                    }`}
+                    onClick={() => setEditing(true)}
+                >
+                    {name}
+                </span>
+                {loading && <progress className="progress w-56"></progress>}
                 <input
-                    disabled={loading}
-                    aria-busy={loading}
                     type="text"
                     placeholder="Category title"
-                    defaultValue={category.name_en}
+                    defaultValue={name}
                     ref={nameFieldRef}
-                    onBlur={updateCategory}
-                    className="input-ghost input text-center text-xl font-bold"
+                    onBlur={() => {
+                        updateCategory();
+                        setEditing(false);
+                    }}
+                    className={`input-ghost input text-center text-xl font-bold ${
+                        !editing && 'hidden'
+                    }`}
                 />
             </div>
         </>
