ea46bce87369ef42f3b658c42e33c3ced9dc847f
suhankins <punishedbernadetta@niuitmo.ru>
2023-04-01 14:27:51 +0400

    fixed accessability issue

--- src/app/admin/Logout.tsx
+++ src/app/admin/Logout.tsx
@@ -4,7 +4,7 @@ import { signOut } from 'next-auth/react';
 
 export function Logout() {
     return (
-        <button onClick={() => signOut()}>
+        <button aria-label="logout" onClick={() => signOut()}>
             <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
