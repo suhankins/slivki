7a1ffcf9a44ca1660b5651b10fb67314d5270fe6
suhankins <punishedbernadetta@niuitmo.ru>
2023-04-03 17:22:58 +0400

    made editabletext its own component

---
+++ src/components/EditableText.tsx
@@ -0,0 +1,61 @@
+import { useRef, useState } from 'react';
+
+export function EditableText({
+    placeholder,
+    defaultValue,
+    fetchUrl,
+    valueName,
+}: {
+    placeholder: string;
+    defaultValue: string;
+    fetchUrl: string;
+    valueName: string;
+}) {
+    const fieldRef = useRef<HTMLInputElement>(null);
+    const [loading, setLoading] = useState(false);
+
+    const updateCategory = () => {
+        if (!fieldRef.current) return;
+        const newValue = fieldRef.current.value.trim();
+        if (newValue === '') {
+            fieldRef.current.value = defaultValue;
+            return;
+        }
+        if (newValue !== defaultValue) {
+            setLoading(true);
+            fetch(fetchUrl, {
+                body: JSON.stringify({ [valueName]: newValue }),
+                method: 'PUT',
+                headers: { 'Content-Type': 'application/json' },
+            }).then(async (res) => {
+                setLoading(false);
+                if (res.status === 200) {
+                    console.log('Category updated');
+                    defaultValue = newValue;
+                    fetch('/api/revalidate'); // Updating main page
+                } else {
+                    console.error('Category update failed');
+                }
+            });
+        }
+    };
+
+    return (
+        <input
+            ref={fieldRef}
+            type="text"
+            disabled={loading}
+            aria-busy={loading}
+            placeholder={placeholder}
+            defaultValue={defaultValue}
+            className={`input-ghost input text-center text-xl font-bold ${
+                loading && 'skeleton'
+            }`}
+            onBlur={() => updateCategory()}
+            onKeyUp={(e) => {
+                console.log(e.key);
+                if (e.key === 'Enter') updateCategory();
+            }}
+        />
+    );
+}
--- src/views/CategoryEditable.tsx
+++ src/views/CategoryEditable.tsx
@@ -1,70 +1,18 @@
 'use client';
 
 import { SimpleCategory } from '@/models/Category';
-import { useEffect, useRef, useState } from 'react';
 import { ItemEditable } from './ItemEditable';
+import { EditableText } from '@/components/EditableText';
 
 export function CategoryEditable({ category }: { category: SimpleCategory }) {
-    const nameFieldRef = useRef<HTMLInputElement>(null);
-    const [name, setName] = useState(category.name_en);
-    const [editing, setEditing] = useState(false);
-    const [loading, setLoading] = useState(false);
-
-    const updateCategory = () => {
-        if (nameFieldRef.current) {
-            const newName = nameFieldRef.current.value.trim();
-            if (newName !== name && newName !== '') {
-                console.log(`Updating category from ${name} to ${newName}`);
-                setLoading(true);
-                fetch(`/api/category/${category._id}`, {
-                    body: JSON.stringify({ name_en: newName }),
-                    method: 'PUT',
-                    headers: { 'Content-Type': 'application/json' },
-                }).then(async (res) => {
-                    setLoading(false);
-                    if (res.status === 200) {
-                        console.log('Category updated');
-                        setName(newName);
-                        fetch('/api/revalidate'); // Updating main page
-                    } else {
-                        console.error('Category update failed');
-                    }
-                });
-            } else {
-                nameFieldRef.current.value = name;
-            }
-        }
-    };
-
-    useEffect(() => {
-        if (editing) nameFieldRef.current?.focus();
-    }, [editing]);
-
     return (
         <div className="flex w-full flex-col items-center gap-4">
             <div className="divider">
-                <span
-                    role="button"
-                    className={`text-center text-xl font-bold ${
-                        (editing || loading) && 'hidden'
-                    }`}
-                    onClick={() => setEditing(true)}
-                >
-                    {name}
-                </span>
-                {loading && <progress className="progress w-56"></progress>}
-                <input
-                    type="text"
-                    placeholder="Category title"
-                    defaultValue={name}
-                    ref={nameFieldRef}
-                    onBlur={() => {
-                        updateCategory();
-                        setEditing(false);
-                    }}
-                    className={`input-ghost input text-center text-xl font-bold ${
-                        !editing && 'hidden'
-                    }`}
+                <EditableText
+                    defaultValue={category.name_en}
+                    placeholder="Category name"
+                    valueName="name_en"
+                    fetchUrl={`/api/category/${category._id}`}
                 />
             </div>
             {category.items &&
