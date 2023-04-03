5864cf6798fb13f86f4c4c46f527983b1e681e8b
suhankins <punishedbernadetta@niuitmo.ru>
2023-04-03 17:23:32 +0400

    made revalidation responses more helpful

--- src/pages/api/revalidate.tsx
+++ src/pages/api/revalidate.tsx
@@ -7,10 +7,16 @@ export default async function handler(
     _req: NextApiRequest,
     res: NextApiResponse
 ) {
+    if (process.env.NODE_ENV === 'development')
+        res.status(200).send(
+            "Can't revalidate in development mode\nhttps://github.com/vercel/next.js/issues/43132"
+        );
     try {
         await res.revalidate('/');
         return res.send('Revalidated');
-    } catch (err) {
-        return res.status(500).send('Error revalidating');
+    } catch (e) {
+        if (e instanceof Error)
+            res.status(500).send(`Error during revalidation: ${e.message}`);
+        return res.status(500).send('Unknown error during revalidation');
     }
 }
