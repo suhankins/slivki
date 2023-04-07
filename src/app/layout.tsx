690b1b435aa36e966a23d2aa23980295f117e325
suhankins <punishedbernadetta@niuitmo.ru>
2023-04-07 20:50:27 +0400

    updated nextjs to 13.3

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,16 @@ import './globals.scss';
 export const metadata = {
     title: 'Slivki',
     description: 'Slivki coffee shop in Batumi, Georgia',
+    colorScheme: 'light',
+    openGraph: {
+        locale: 'en_US',
+        siteName: 'Slivki',
+        description: 'Slivki coffee shop in Batumi, Georgia',
+        images: [
+            // TODO: Add actual images
+            'https://storage.googleapis.com/slivki_pictures-f3fd48a/latte.webp',
+        ],
+    },
 };
 
 export default function RootLayout({
