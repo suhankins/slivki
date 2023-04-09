caaa9c4e3bfc932a7bf85b55e2b7d6685dc68a2b
suhankins <punishedbernadetta@niuitmo.ru>
2023-04-09 16:53:54 +0400

    added config to accept images from google cloud

--- next.config.js
+++ next.config.js
@@ -3,6 +3,16 @@ const nextConfig = {
     experimental: {
         appDir: true,
     },
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'storage.googleapis.com',
+                port: '',
+                pathname: `/${process.env.BUCKET_NAME}/**`,
+            },
+        ],
+    },
 };
 
 module.exports = nextConfig;
