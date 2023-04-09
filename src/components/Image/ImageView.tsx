5342f84a90103b7dcdcfa78cc6809f6a35e4fcc8
suhankins <punishedbernadetta@niuitmo.ru>
2023-04-09 16:55:36 +0400

    added high priority to ImageView to optimize LCP

--- src/components/Image/ImageView.tsx
+++ src/components/Image/ImageView.tsx
@@ -11,6 +11,7 @@ export function ImageView({
         <Image
             width={312} // Resolution of the image at 4K display
             height={312}
+            priority={true}
             alt={altText ?? ''}
             className="aspect-square h-full w-full rounded-lg bg-base-300 object-cover sm:row-span-2"
             src={picture}
