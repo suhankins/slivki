6689c85e1295385b0991b89185b4a9ffcf902133
suhankins <punishedbernadetta@niuitmo.ru>
2023-04-10 12:55:24 +0400

    replaced jsx icons with heroicons library

--- src/components/Logout.tsx
+++ src/components/Logout.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { signOut } from 'next-auth/react';
+import { ArrowLeftOnRectangleIcon } from '@heroicons/react/24/outline';
 
 export interface LogoutProps {
     className?: string;
@@ -14,20 +15,7 @@ export function Logout({ className }: LogoutProps) {
             className={`btn-ghost btn-square btn relative ${className}`}
             onClick={() => signOut()}
         >
-            <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="absolute h-6 w-6"
-            >
-                <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75"
-                />
-            </svg>
+            <ArrowLeftOnRectangleIcon className="absolute h-6 w-6" />
         </button>
     );
 }
