c6ca0a7de7434b87fe417900606697e8c81168df
suhankins <punishedbernadetta@niuitmo.ru>
2023-04-11 14:08:42 +0400

    added better image deletion

--- src/app/api/category/[id]/[itemIndex]/[field]/route.ts
+++ src/app/api/category/[id]/[itemIndex]/[field]/route.ts
@@ -2,6 +2,47 @@ import { NextRequest, NextResponse } from 'next/server';
 import { getBodyAndCategory } from '../../getBodyAndCategory';
 import { handleDbError } from '@/utils/server/handleDbError';
 import { ItemClass } from '@/models/Item';
+import { findCategory } from '@/utils/server/findCategory';
+import { googleStorage } from '@/lib/googleStorage';
+
+export async function DELETE(
+    _request: NextRequest,
+    {
+        params: { id, itemIndex, field },
+    }: { params: { id: string; itemIndex: number; field: string } }
+) {
+    // Currently, this function is only meant for deleting images
+    if (field !== 'image')
+        return new NextResponse('Invalid field', { status: 400 });
+
+    const category = await findCategory(id);
+    if (category instanceof NextResponse) return category;
+
+    if (category.items === undefined || category.items.length === 0)
+        return new NextResponse('No items in category', { status: 400 });
+
+    if (isNaN(itemIndex) || itemIndex < 0 || itemIndex >= category.items.length)
+        return new NextResponse('Invalid item index', { status: 400 });
+
+    const image = category.items[itemIndex].image;
+    if (image === undefined)
+        return new NextResponse('No image to delete', { status: 400 });
+    const parts = image.split('/');
+    const filename =
+        parts[parts.length - 1] === ''
+            ? parts[parts.length - 2]
+            : parts[parts.length - 1];
+
+    try {
+        category.items[itemIndex].image = undefined;
+        await category.save();
+        googleStorage.file(filename).delete();
+    } catch (e) {
+        return handleDbError(e);
+    }
+
+    return new NextResponse(`${field} successfully updated`, { status: 200 });
+}
 
 export async function PUT(
     request: NextRequest,
@@ -9,11 +50,6 @@ export async function PUT(
         params: { id, itemIndex, field },
     }: { params: { id: string; itemIndex: number; field: string } }
 ) {
-    console.log('PUT /api/category/:id/:itemIndex/:field', {
-        id,
-        itemIndex,
-        field,
-    });
     if (!(field in ItemClass))
         return new NextResponse('Invalid field', { status: 400 });
     const key = field as keyof ItemClass;
--- src/utils/client/image/deleteImage.ts
+++ src/utils/client/image/deleteImage.ts
@@ -1,12 +1,7 @@
 export async function deleteImage(categoryId: string, itemIndex: number) {
     const result = await fetch(
         `/api/category/${categoryId}/${itemIndex}/image`,
-        {
-            method: 'PUT',
-            body: JSON.stringify({
-                value: '',
-            }),
-        }
+        { method: 'DELETE' }
     );
     if (result.ok) console.log('Deleted successfully!');
     else throw new Error('Failed to delete.');
