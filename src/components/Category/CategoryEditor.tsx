6589d50d53c85dcd0f48c5d44e57887ed73371a7
suhankins <punishedbernadetta@niuitmo.ru>
2023-04-14 13:47:33 +0400

    added delete button to categoryeditor

--- src/components/Category/CategoryEditor.tsx
+++ src/components/Category/CategoryEditor.tsx
@@ -3,17 +3,21 @@ import { ItemEditor } from '../Item/ItemEditor';
 import { EditableText } from '@/components/EditableText';
 import { Category } from './Category';
 import { NewItem } from '../Item/NewItem';
+import { DeleteButton } from '../DeleteButton';
 
 export function CategoryEditor({ category }: { category: SimpleCategory }) {
     return (
         <Category
             title={
-                <EditableText
-                    defaultValue={category.name}
-                    fetchUrl={`/api/category/${category._id}/name`}
-                    className="input-ghost input text-center text-xl font-bold"
-                    placeholder="Category name"
-                />
+                <>
+                    <EditableText
+                        defaultValue={category.name}
+                        fetchUrl={`/api/category/${category._id}/name`}
+                        className="input-bordered input w-48 text-center text-xl font-bold xs:w-64"
+                        placeholder="Category name"
+                    />
+                    <DeleteButton fetchUrl={`/api/category/${category._id}`} />
+                </>
             }
         >
             {category.items &&
