922cf8bb79cf9c38ddb648e9f270b58c1d3ab455
suhankins <punishedbernadetta@niuitmo.ru>
2023-04-18 19:50:52 +0400

    removed priority from item images

--- src/components/Image/ImageView.tsx
+++ src/components/Image/ImageView.tsx
@@ -11,7 +11,6 @@ export function ImageView({
         <Image
             width={312} // Resolution of the image at 4K display
             height={312}
-            priority={true}
             alt={altText ?? ''}
             className="aspect-square h-full w-full rounded-lg bg-base-300 object-cover sm:row-span-2"
             src={picture}
