d66d47275e2cf61f292adc5b42dfbd6bf72ca7ff
suhankins <punishedbernadetta@niuitmo.ru>
2023-04-21 14:03:33 +0400

    allowed text to be longer if there's no image

--- src/components/Item/Item.tsx
+++ src/components/Item/Item.tsx
@@ -17,7 +17,11 @@ export function Item({
         <article className="relative grid w-full max-w-2xl grid-cols-2 gap-4 rounded-lg bg-base-200 p-4">
             {children}
             {image}
-            <header className="flex flex-col gap-4">
+            <header
+                className={`flex flex-col gap-4 ${
+                    !image && 'col-span-2 sm:col-span-1'
+                }`}
+            >
                 {title}
                 {description}
             </header>
