b963293faa9f7f0c5b0691b624df00d6ef9413f3
suhankins <punishedbernadetta@niuitmo.ru>
2023-05-02 18:51:42 +0400

    added chevron on LanguagePickerViewer

--- src/components/LanguagePicker/LanguagePickerViewer.tsx
+++ src/components/LanguagePicker/LanguagePickerViewer.tsx
@@ -1,4 +1,5 @@
 import { Locale, i18n } from '@/lib/i18n-config';
+import { ChevronDownIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 
 export function LanguagePickerViewer({
@@ -9,8 +10,9 @@ export function LanguagePickerViewer({
     const languages = i18n.locales;
     return (
         <div className="dropdown-end dropdown ml-auto">
-            <label tabIndex={0} className="btn-ghost btn m-1">
-                {selectedLang}
+            <label tabIndex={0} className="btn-ghost btn m-1 flex gap-2">
+                <span>{selectedLang}</span>
+                <ChevronDownIcon className="h-4 w-4" />
             </label>
             <ul
                 tabIndex={0}
@@ -19,7 +21,7 @@ export function LanguagePickerViewer({
                 {languages.map(
                     (locale) =>
                         locale !== selectedLang && (
-                            <li>
+                            <li key={locale}>
                                 <Link href={`/${locale}`}>{locale}</Link>
                             </li>
                         )
