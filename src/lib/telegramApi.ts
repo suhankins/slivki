e55a170990425d31f1eed594a5a3d32345a3eaed
suhankins <punishedbernadetta@niuitmo.ru>
2023-05-08 11:05:36 +0400

    added debug logs in sendMessage

--- src/lib/telegramApi.ts
+++ src/lib/telegramApi.ts
@@ -1,12 +1,14 @@
 export const telegramUrl =
     'https://api.telegram.org/bot' + process.env.TELEGRAM_TOKEN + '/';
 
-export function sendMessage(chat_id: string, text: string) {
-    return fetch(telegramUrl + 'sendMessage', {
+export async function sendMessage(chat_id: string, text: string) {
+    const result = await fetch(telegramUrl + 'sendMessage', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
         },
         body: JSON.stringify({ chat_id, text }),
     });
+    console.log('Sent message to telegram. Response:', result);
+    return result;
 }
