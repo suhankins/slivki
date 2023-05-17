b43ef09d60f3d587a43d5259e3cf923d135e6969
suhankins <punishedbernadetta@niuitmo.ru>
2023-05-17 22:02:48 +0400

    optimized navbar styles

--- src/components/Drawer/Navbar.tsx
+++ src/components/Drawer/Navbar.tsx
@@ -2,6 +2,7 @@
 
 import { useScrollPosition } from '@/hooks/useScrollPosition';
 import { isBrowser } from '@/utils/client/isBrowser';
+import { useMemo } from 'react';
 
 export function Navbar({
     children,
@@ -13,15 +14,19 @@ export function Navbar({
     changeOnScroll?: boolean;
 }) {
     const scrollPosition = useScrollPosition(parentId);
+    const isBelowThreshold = useMemo(
+        () =>
+            !changeOnScroll ||
+            (isBrowser() &&
+                scrollPosition >= (window.visualViewport?.height ?? 1080) - 64),
+        [scrollPosition]
+    );
     return (
         <nav
             className={`navbar sticky top-0 left-0 z-40 ${
-                changeOnScroll &&
-                (!isBrowser() ||
-                    scrollPosition <
-                        (window.visualViewport?.height ?? 1080) - 64)
-                    ? 'bg-transparent text-white'
-                    : 'bg-base-300'
+                isBelowThreshold
+                    ? 'bg-base-300/100 text-neutral'
+                    : 'bg-base-300/0 text-white'
             }`}
         >
             {children}
