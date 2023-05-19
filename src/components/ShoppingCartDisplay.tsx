a69dfdbecb13af2bd3a66b8f2090e8f65181bfc4
suhankins <punishedbernadetta@niuitmo.ru>
2023-05-19 21:59:12 +0400

    added ping animation to adding to cart

--- src/components/ShoppingCartDisplay.tsx
+++ src/components/ShoppingCartDisplay.tsx
@@ -1,28 +1,41 @@
 'use client';
 
 import ShoppingCartIcon from '@heroicons/react/24/outline/ShoppingCartIcon';
-import { useContext, useMemo } from 'react';
+import { useContext, useMemo, useRef } from 'react';
 import { CartContentsContext } from './CartProvider';
 
 export function ShoppingCartDisplay() {
     const cart = useContext(CartContentsContext);
-    const cartItemCount = useMemo(
-        () => cart.reduce((a, b) => a + (b.quantity ?? 1), 0),
-        [cart]
+    const buttonRef = useRef<HTMLButtonElement>(null);
+
+    const cartItemCount = useMemo(() => {
+        buttonRef?.current?.classList.add('animate-ping-once');
+        return cart.reduce((a, b) => a + (b.quantity ?? 1), 0);
+    }, [cart]);
+
+    const handleAnimationEnd = useMemo(
+        () => () => {
+            buttonRef?.current?.classList.remove('animate-ping-once');
+        },
+        []
     );
 
     return (
-        <div className="indicator">
-            <span className="indicator-start indicator-bottom badge-secondary badge indicator-item bottom-1">
-                {cartItemCount}
-            </span>
-            <button
-                className={`animate-bounce-once btn-ghost btn-square btn relative`}
-                type="button"
-                aria-label="Shopping cart"
-            >
-                <ShoppingCartIcon className="absolute h-6 w-6" />
-            </button>
-        </div>
+        <button
+            className="btn-ghost btn-square btn relative mr-3"
+            type="button"
+            aria-label="Shopping cart"
+            ref={buttonRef}
+            onAnimationEnd={handleAnimationEnd}
+        >
+            <div className="indicator absolute">
+                {cartItemCount !== 0 && (
+                    <span className="indicator-start indicator-bottom badge-secondary badge indicator-item">
+                        {cartItemCount}
+                    </span>
+                )}
+                <ShoppingCartIcon className="h-6 w-6" />
+            </div>
+        </button>
     );
 }
