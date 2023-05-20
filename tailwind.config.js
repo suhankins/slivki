4a1e212cba672ff19882d7934128e93d8f0e1624
suhankins <punishedbernadetta@niuitmo.ru>
2023-05-20 17:09:33 +0400

    changed theme replacement to extend

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,9 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
     content: ['./src/**/*.{js,ts,jsx,tsx}'],
-    theme: {
+    extend: {
         screens: {
             xs: '375px',
-            ...defaultTheme.screens,
         },
     },
     plugins: [require('daisyui')],
