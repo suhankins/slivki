c2745cc2541dadf7b1a5caa5ea24d9f6246971bc
suhankins <punishedbernadetta@niuitmo.ru>
2023-05-29 17:17:28 +0400

    added better logging registering telegram webhook

--- src/app/telegram/setup/route.ts
+++ src/app/telegram/setup/route.ts
@@ -5,6 +5,9 @@ import { telegramUrl } from '@/lib/telegramApi';
  * Doesn't need to be called manually, building the project will do it automatically.
  */
 export async function GET() {
+    if (process.env.NODE_ENV === 'development')
+        return new Response('Cannot set webhook in development mode');
+
     const result = await fetch(telegramUrl + 'setWebhook', {
         method: 'POST',
         headers: {
@@ -16,7 +19,12 @@ export async function GET() {
             allowed_updates: ['message'],
             drop_pending_updates: true,
         }),
+    }).catch((err) => {
+        console.error('Failed to register a webhook', err);
+        return new Response('Failed to register a webhook', { status: 500 });
     });
+
     const text = await result.text();
+    console.log('Registering webhook result:', text);
     return new Response(text);
 }
