dd2994134630047765780b8433364e878ac92d76
suhankins <punishedbernadetta@niuitmo.ru>
2023-06-02 21:31:29 +0400

    moved order form out of cart components

--- src/app/[lang]/checkout/page.tsx
+++ src/app/[lang]/checkout/page.tsx
@@ -1,5 +1,5 @@
 import { CartViewer } from '@/components/Cart/CartViewer';
-import { OrderForm } from '@/components/Cart/OrderForm';
+import { OrderForm } from '@/components/OrderForm';
 import { getDictionary } from '@/lib/getDictionary';
 import { Locale, i18n } from '@/lib/i18n-config';
 
--- src/components/OrderForm.tsx
+++ src/components/OrderForm.tsx
@@ -11,7 +11,7 @@ import {
     useMemo,
     useState,
 } from 'react';
-import { CartActionContext, CartContentsContext } from './CartProvider';
+import { CartActionContext, CartContentsContext } from './Cart/CartProvider';
 import ReCAPTCHA from 'react-google-recaptcha';
 import { useRouter } from 'next/navigation';
 import { isWorkingHours } from '@/utils/isWorkingHours';
